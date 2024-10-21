dc8cf13ee221c6e2736d617384e380ae91d691c1
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-10-21 22:15:46 +0500

    added rectangle code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,75 @@
-const canvas = document.querySelector('canvas')
+// Global variables
+const canvas = document.querySelector('canvas'),
+  toolBtns = document.querySelectorAll('.tool')
 
+//  Variable
 let ctx = canvas.getContext('2d'),
-isDrawing = false
-brushWidth = 5
+isDrawing = false,
+brushWidth = 5,
+selectedTool = 'brush',
+prevMouseX,
+prevMouseY,
+snapshot
 
+
+// Set canvas width and height
 window.addEventListener('load', () => {
   canvas.width = canvas.offsetWidth
   canvas.height = canvas.offsetHeight
 })
 
-const startDraw = () => {
+// Start drawing
+const startDraw = e => {
 isDrawing = true
+prevMouseX = e.offsetX
+prevMouseY = e.offsetY
 ctx.beginPath()
 ctx.lineWidth = brushWidth
-}
-const drawing = e => {
-  if(!isDrawing) return
-  ctx.lineTo(e.offsetX, e.offsetY)
-  ctx.stroke()
+snapshot= ctx.getImageData(0, 0, canvas.width, canvas.height)
+console.log(snapshot);
 }
 
+// draw rectangle
+const drawRectangle = e => {
+  ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
+}
+
+// drawing
+const drawing = e => {
+  if(!isDrawing) return
+  ctx.putImageData(snapshot, 0, 0)
+  switch (selectedTool) {
+    case 'brush':
+      ctx.lineTo(e.offsetX, e.offsetY)
+      ctx.stroke()
+      break
+    case 'rectangle':
+      drawRectangle(e)
+      break
+      default:
+      break
+  }
+
+
+  
+}
+
+// tolls btn and set to variables selected tool
+toolBtns.forEach (btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.options .active').classList.remove('active')
+    btn.classList.add('active')
+    selectedTool = btn.id
+    console.log(`Selected tool ${selectedTool}`);
+  })
+})
+
+// stop 
 const stopDraw = () => {
   isDrawing = false
 }
+
 canvas.addEventListener('mousedown', startDraw)
 canvas.addEventListener('mousemove', drawing)
 canvas.addEventListener('mouseup', stopDraw)
+
